07addab616ae5829813035b6dbb513219c8e098e
shapievabib-afk <shapiev.abib@gmail.com>
2025-09-23 10:13:12 +0300

    Добавлен двоичный счетчик(1010-бета версия и не работает)

--- get-led/1010-counter.py
+++ get-led/1010-counter.py
@@ -1,10 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
+
 GPIO.setmode(GPIO.BCM)
 leds= [24, 22, 23, 27, 17, 25, 12, 16]
 GPIO.setup(leds, GPIO.OUT)
 GPIO.output(leds, 0)
+GPIO.input(9, GPIO.IN)
+GPIO.input(10, GPIO.IN)
+
 num = 0
 
 def dec2bin(value):
@@ -13,6 +17,11 @@ def dec2bin(value):
 sleep_time = 0.2
 
 if GPIO.input(up):
+    num = num + 1
+    print(num, dec2bin(num))
+    time.sleep(sleep_time)
+
+else GPIO.input(down):
     num = num + 1
     print(num, dec2bin(num))
     time.sleep(sleep_time)
---
+++ get-led/bin-counter.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+leds= [16, 12, 25, 17, 27, 23, 22, 24]
+up = 9
+down = 10
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(leds, GPIO.OUT)
+GPIO.setup(up, GPIO.IN)
+GPIO.setup(down, GPIO.IN)
+num = 0
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+sleep_time = 0.2
+
+while True:
+    if GPIO.input(up):
+        num<=8
+        num = num+1
+        print(num, dec2bin(num))
+        time.sleep(sleep_time)
+    if GPIO.input(down):
+        num = num -1
+        print(num, dec2bin(num))
+        time.sleep(sleep_time)
+
+    GPIO.output(leds, dec2bin(num))
+        
