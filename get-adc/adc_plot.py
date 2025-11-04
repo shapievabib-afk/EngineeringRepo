cbabf1ba8e96c79571c077adb1f3e763f98b7d84
Khalid Shapiev <shapiev.abib@gmail.com>
2025-11-04 09:37:00 +0300

    Update adc_plot.py
    добавлен plot_sampling_period

--- get-adc/adc_plot.py
+++ get-adc/adc_plot.py
@@ -6,6 +6,14 @@ def plt_voltage_vs_time(time, voltage, max_voltage):
     plt.ylabel("Voltage")
     plt.xlabel("Time")
 
+def plot_sampling_period_hist(time):
+    old_time = time
+    sampling_period = time - old_time
+    plt.figure(figsize=(10, 6))
+    plt.hist(sampling_period)
+    plt.xlim(0, 0.06)
+    plt.grid(True)
+
     plt.show()
 
     #part if cod that is needed to fix
