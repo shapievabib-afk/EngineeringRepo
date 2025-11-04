75e1f2de036d7e3ae6e57a26c6c0fa40509de099
Khalid Shapiev <shapiev.abib@gmail.com>
2025-11-04 09:43:33 +0300

    Update r2r_sc.py
    added plot

--- get-adc/r2r_sc.py
+++ get-adc/r2r_sc.py
@@ -1,4 +1,3 @@
-
 import time
 import adc_plot as plot
 import r2r_adc
@@ -7,25 +6,18 @@ adc = r2r_adc.R2R_ADC(3.29, 0.0001)
 voltage_values = []
 time_values = []
 duration = 3.0
-
 try:
     start = time.time()
     now = time.time()
     counter = 0
     while now-start<=duration:
-            
         now = time.time()
         voltage_values.append(adc.get_sc_voltage())
         time_values.append(now- start)
-
     plot.plt_voltage_vs_time(time_values, voltage_values, 3.3)
-
-
-
+    plot.plot_sampling_period_hist(time_values)
 finally:
         adc.deinit()
-        
-        #need to fix... still
 
 
 
