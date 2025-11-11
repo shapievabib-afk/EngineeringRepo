1ca5771d97d6c10c8661716c0d5217df4e48fbca
Khalid Shapiev <shapiev.abib@gmail.com>
2025-11-11 11:45:04 +0300

    Update adc_plot.py

--- get-adc/adc_plot.py
+++ get-adc/adc_plot.py
@@ -1,19 +1,27 @@
 import matplotlib.pyplot as plt
-
-def plt_voltage_vs_time(time, voltage, max_voltage):
-    plt.figure(figsize=(10, 6))
+def plot_voltage_vs_time(time, voltage, max_voltage):
+    plt.figure(figsize=(10,6))
     plt.plot(time, voltage)
-    plt.ylabel("Voltage")
-    plt.xlabel("Time")
-
+    plt.xlabel("Время")
+    plt.ylabel("Напряжение")
+    plt.grid(True, which="major", linestyle="-")
+    plt.grid(True, which="minor", linestyle="--", linewidth=0.5)
+    plt.ylim(0, max_voltage)
+    plt.minorticks_on()
+    plt.show()
 def plot_sampling_period_hist(time):
-    old_time = time
-    sampling_period = time - old_time
-    plt.figure(figsize=(10, 6))
-    plt.hist(sampling_period)
+    sampling_periods=[]
+    for i in range(len(time)-1):
+        sampling_periods.append(time[i+1]-time[i])
+    plt.figure(figsize=(10,6))
+    plt.hist(sampling_periods)
+    plt.title("Распределение количества измерений по их продолжительности")
+    plt.xlabel("Время")
+    plt.ylabel("Количество измерений")
     plt.xlim(0, 0.06)
-    plt.grid(True)
-
+    plt.grid(True, which="major", linestyle="-")
+    plt.grid(True, which="minor", linestyle="--", linewidth=0.5)
+    plt.minorticks_on()
     plt.show()
 
     #part if cod that is needed to fix
